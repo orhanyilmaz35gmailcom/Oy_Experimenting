0dbde1c5d19a00b8a2ecf060d64588da12e719a2
orhan Y <ooorhanyilmaz35@gmail.com>
2023-06-20 21:57:43 +0200

    1

--- src/main/java/Oy_ex_Packages01/Oy_ex_String_Manipulations02.java
+++ src/main/java/Oy_ex_Packages01/Oy_ex_String_Manipulations02.java
@@ -6,17 +6,6 @@ public class Oy_ex_String_Manipulations02 {
     public static void main(String[] args) {
 
 
- /*
-        Ornek:
-            Asagidaki kurallara gore kullanicinin girdigi password u kontrol ediniz
-                1) En az 8 charcter olsun
-                2) Space characteri passwordde olmasin
-                3) En az bir buyuk harf olsun
-                4) En az bir kucuk harf olsun
-                5) En az bir rakam olsun
-         */
-
-
         Scanner input =new Scanner(System.in);
         System.out.println("Lütfen parolanizi giriniz: ");
         String pwd= input.nextLine();
